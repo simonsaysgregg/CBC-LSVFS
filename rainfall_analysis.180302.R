153e6876cca8b6ca539d45739f1bfd8e9072e0ea
Simon Gregg <simongregg@Simons-iMac.local>
2018-04-16 14:24:10 -0400

    Cleaned code; established abliltiy to summarise df in list

--- rainfall_analysis.180302.R
+++ rainfall_analysis.180302.R
@@ -1,34 +1,40 @@
-####### Analysis in project file
-#Visualizing data
+## Analysis of LSVFS data from calvary baptist church
+## monitoring period August 2017-November 2017
+## data location in CBC_LSVFS.DEL.csv
+
+## Visualizing data
 require("ggplot2")      # Powerful and aesthetic plotting system for R
 require("gridExtra")    # Arrange multiple ggplots in same figure (multi-panels)
 require("scales")       #
 require("RColorBrewer") # creates nice color schemes
 require("corrplot")     # A graphical display of a correlation matrix between all combinations of variables
-#Statistical analysis
+## Statistical analysis
 require("stats")        # Lots of stats stuff
-#Data management
+## Data management
 require("plyr")         # Allows you t split data structure into groups (pollutant type, location, etc.) and apply function on each group
 require("dplyr")
 require("zoo")          # Helps streamline data if you have irregular time series
 require("reshape2")     # Convert data with "wide" columns to "long" columns
 require("lubridate")    # Date and time data made easy! See reference PDF in Google Drive
 require("data.table")
-require("xlsx")         # Reads and writes to xlsx file
+#require("xlsx")        # creates errors # Reads and writes to xlsx file
 require("purrr")
 require("tidyr")
-#Mapping tools
+require("fBasics")
+## Mapping tools
 require("stringi")
 require("ggmap")        # Plotting of maps same as you would with ggplot2
 require("maptools")     # Read, write, and handle Shapefiles in R
 require("mapdata")      # Supplement to maps package
-###### read the file
+
+## Read the file
 LSVFS.1 <- read.csv("CBC_LSVFS.DEL.csv")
-# reformat dates
-LSVFS.1$date.time <- as.POSIXct(CBC_LSVFS.1$date.time, 
+
+## Reformat dates
+LSVFS.1$date.time <- as.POSIXct(LSVFS.1$date.time, 
                                 format = "%m/%d/%y %H:%M", tz = "est")
-# Rename columns
-# Nameing that will differeintiate in plot legend
+## Rename columns
+## Nameing that will differeintiate in plot legend
 colnames(LSVFS.1) <- c("Date", 
                        "Rainfall", 
                        "Intensity", 
@@ -41,34 +47,37 @@ colnames(LSVFS.1) <- c("Date",
 ## View
 #View(LSVFS.1)
 
-###### Exstract Date & Rainfall
+## Exstract Date & Rainfall
 RainData <- LSVFS.1 %>%
-            select("Date", "Rainfall") 
+            select("Date",
+                   "Rainfall") 
 #View(RainData)
 
-###### Replace NA with 0
+## Replace NA with 0
 RainData[is.na(RainData)] <- 0
-### View
+## View
 #View(RainData)
 
-###### Delineate Events
-### Code found online
+## Delineate Events
+# Code found online
 # column 1 is time stamp in POSIXct format, 
-# column 2 is rainfall depth. My data is collected at two minute intervals; 
-# Some data hourly. 
+# column 2 is rainfall depth; current interval: two minute
+# Change accordingly, units minutes
 Rain_Over_0<- RainData[RainData[,2]!=0,]  
 # Create vector increasing by 1 as Diff=>60 (Time specific) 
-# change value of Diff here to change the MIT.
-# input your value of MIT (in minutes) where the code says 30.
+# change value of Diff here to change the Minimum Interval Time (MIT)
+# input your value of MIT (in minutes); current MIT: 60.
 Rainindex<-c(0,cumsum(diff(Rain_Over_0[,1])>60)) 
 # Split into list of events
-RainEvents<-split(Rain_Over_0, Rainindex) # this returns a list of events. You can then use sapply functions to determine the rain statistics you need. 
+RainEvents<-split(Rain_Over_0, Rainindex) 
+# Returns a list of events 
+# Use sapply functions to determine the rain statistics 
 #View(RainEvents)
 
-###### Unlist events 
-ul.RainEvents <- RainEvents %>%
-                 unnest() 
-# Error in function_list[[k]](value): Could not find function "unnest"
-
-
+## Calculates mean of Duration & Rainfall Accumulaiton 
+# Returns a data fram of values same length as list
+Rainsum <- RainEvents %>%
+        map_df(function(df) {summarise(df, Duration = max(Date)-min(Date),
+                                           Accumulation = sum(Rainfall))})
+#View(Rainsum)
 
